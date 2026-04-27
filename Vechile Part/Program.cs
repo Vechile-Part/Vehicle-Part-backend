742106ca9bd5ad5ed3536e3de4b36595f0236d37
dashmashrestha <dashma657@gmail.com>
2026-04-27 15:55:37 +0545

    feat: feature 14 - purchase history

--- Vechile Part/Program.cs
+++ Vechile Part/Program.cs
@@ -1,26 +1,23 @@
 using VechilePart.Infrastructure;
+using VechilePart.Application.Interfaces;
+using VechilePart.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddInfrastructure();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddScoped<ICustomerFeatureService, CustomerFeatureService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
