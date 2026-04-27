96ba7e54fec9d6a8148434c2be9a13dbfd1258e3
dashmashrestha <dashma657@gmail.com>
2026-04-27 15:54:23 +0545

    feat: feature 13 - book appointment, request part, review service

--- VechilePart.Infrastructure/Data/AppDbContext.cs
+++ VechilePart.Infrastructure/Data/AppDbContext.cs
@@ -1,17 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using VechilePart.Application.Interfaces;
 using VechilePart.Domain.Entities;
 
 namespace VechilePart.Infrastructure.Data;
 
-public class AppDbContext
+public class AppDbContext : DbContext, IAppDataStore
 {
-    public List<User> Users { get; } = [];
-    public List<Customer> Customers { get; } = [];
-    public List<Vehicle> Vehicles { get; } = [];
-    public List<Vendor> Vendors { get; } = [];
-    public List<Part> Parts { get; } = [];
-    public List<PurchaseInvoice> PurchaseInvoices { get; } = [];
-    public List<SalesInvoice> SalesInvoices { get; } = [];
-    public List<Appointment> Appointments { get; } = [];
-    public List<PartRequest> PartRequests { get; } = [];
-    public List<ServiceReview> ServiceReviews { get; } = [];
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Customer> Customers => Set<Customer>();
+    public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+    public DbSet<Vendor> Vendors => Set<Vendor>();
+    public DbSet<Part> Parts => Set<Part>();
+    public DbSet<PurchaseInvoice> PurchaseInvoices => Set<PurchaseInvoice>();
+    public DbSet<SalesInvoice> SalesInvoices => Set<SalesInvoice>();
+    public DbSet<Appointment> Appointments => Set<Appointment>();
+    public DbSet<PartRequest> PartRequests => Set<PartRequest>();
+    public DbSet<ServiceReview> ServiceReviews => Set<ServiceReview>();
+
+    List<Appointment> IAppDataStore.Appointments => Appointments.ToList();
+    List<PartRequest> IAppDataStore.PartRequests => PartRequests.ToList();
+    List<ServiceReview> IAppDataStore.ServiceReviews => ServiceReviews.ToList();
+    List<SalesInvoice> IAppDataStore.SalesInvoices => SalesInvoices.ToList();
+    void IAppDataStore.Add<T>(T entity) => base.Add(entity);
+    void IAppDataStore.SaveChanges() => base.SaveChanges();
 }
