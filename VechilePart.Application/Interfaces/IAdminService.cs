0e8e30fcd27ed4588d0dc26e2b33756c83da760e
sujanpaudel658 <sujanpaudel368@gmail.com>
2026-04-27 13:35:08 +0545

    Implement Admin Financial Reports with validation and date filters

--- VechilePart.Application/Interfaces/IAdminService.cs
+++
@@ -1,8 +0,0 @@
-using VechilePart.Application.DTOs;
-
-namespace VechilePart.Application.Interfaces;
-
-public interface IAdminService
-{
-    Task<FinancialReportDto> GetFinancialReportAsync(string reportType, CancellationToken cancellationToken = default);
-}
