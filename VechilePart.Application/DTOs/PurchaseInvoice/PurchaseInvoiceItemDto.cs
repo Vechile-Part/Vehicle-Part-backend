a6306446c3909dfa4267d8b1a161f06ef905c250
yojana9 <yojanasubedi66@gmail.com>
2026-05-06 18:31:31 +0545

    feat(purchase-invoice): implement stock purchase invoice feature

---
+++ VechilePart.Application/DTOs/PurchaseInvoice/PurchaseInvoiceItemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace VehiclePart.Application.DTOs.PurchaseInvoice;
+
+public class PurchaseInvoiceItemDto
+{
+    public Guid PartId { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+}
