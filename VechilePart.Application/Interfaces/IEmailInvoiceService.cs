75566dd3143b76de94f73f3a39cf7b6e63630b87
yojana9 <yojanasubedi66@gmail.com>
2026-05-08 12:03:17 +0545

    feature 11 completed

---
+++ VechilePart.Application/Interfaces/IEmailInvoiceService.cs
@@ -0,0 +1,8 @@
+﻿using VehiclePart.Application.DTOs;
+
+namespace VehiclePart.Application.Interfaces;
+
+public interface IEmailInvoiceService
+{
+    Task<bool> SendInvoiceEmailAsync(EmailInvoiceDto dto);
+}
